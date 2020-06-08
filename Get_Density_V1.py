2d6763f7361c0c41ce25098eb8b43b466722fcc7
Copple <gcopple@intra.sanborn.com>
2020-06-08 15:39:45 -0600

    version one complete

--- Get_Density_V1.py
+++ Get_Density_V1.py
@@ -1,1 +1,17 @@
 import os
+import time
+if os.path.exists("Spacing_Only.txt"):
+    os.remove("Spacing_Only.txt")
+    print("Old spacing file removed, Creating new file.")
+else:
+    print("Creating file")
+with open("Density_and_Spacing.txt", "r") as f:
+    lines = f.readlines()
+    for X in lines:
+        if "spacing" in X:
+            with open("Spacing_Only.txt", "a") as of:
+                Spacing_List = X.split(" ")
+                Spacing = Spacing_List[Spacing_List.index("only") + 1]
+                of.write(Spacing + '\n')
+print("File has been created. Thank you.")
+time.sleep(8)
